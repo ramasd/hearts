b57e65ecd2e619531c030a0eb18d2394d5cb317b
ramasd <ramunas.danupas@gmail.com>
2021-11-28 00:30:47 +0200

    Display played cards with nickname and color, played cards amount, current player points

--- resources/views/cards.blade.php
+++ resources/views/cards.blade.php
@@ -4,32 +4,117 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    <title>Cards</title>
+    <title>Played Cards</title>
 
     <style>
+        body{
+            font-family: 'Brush Script MT', cursive;
+        }
+
+        h1{
+            text-decoration: underline;
+        }
+
+        table{
+            margin-bottom: 20px;
+            font-size: 20px;
+        }
+
+        th{
+            text-align: left;
+        }
+
         .opacity{
             opacity: 0.25;
-            padding: 3px;
+            padding: 2px;
         }
 
         .border{
             border-style: solid;
-            border-color: yellowgreen;
-            border-width: 3px;
-            border-radius: 11px;
+            border-color: darkgrey;
+            border-width: 2px;
+            border-radius: 8px 8px 0 0;
+            background-color: darkgrey;
+        }
+
+        .card-size{
+            height: 120px;
+            width: 80px;
+        }
+
+        .player-name {
+            font-size: small;
+            text-align: center;
+            font-weight: bold;
+            width: 72px;
+            overflow: hidden;
+            padding: 0 6px;
+            margin-top: -6px;
+            position: relative;
+            background-color: darkgrey;
+            border-radius: 0 0 8px 8px;
+            color: darkgreen;
+        }
+
+        .played-cards-amount{
+            font-size: 20px;
+            color: darkslategray;
+            padding-left: 20px;
+            font-weight: bold;
+        }
+
+        .played-cards-amount span{
+            color: darkgrey;
+        }
+
+        .player-points{
+            border: solid;
+            text-align: center;
+        }
+
+        .align-top{
+            vertical-align: top;
         }
     </style>
 </head>
 <body>
-<div>
-    @foreach($cards as $suit => $types)
-        <div>
-            @foreach($types as $type)
-                @php $card = $suit . "_" . $type; @endphp
-                <img class="{{ in_array($card, $playedCards) ? 'border' : "opacity" }}"  src="{{ url("images/cards/$card.svg") }}" alt="{{$card}}" height="150" width="100"/>
+    <h1>Played Cards</h1>
+    <div>
+        <table>
+            @foreach($cards as $suit => $types)
+                <tr>
+                    @foreach($types as $type)
+                        @php $card = $suit . "_" . $type; @endphp
+                        <td class="align-top">
+                            <div>
+                                <div>
+                                    <img class="card-size {{ array_key_exists($card, $playedCards) ? 'border' : "opacity" }}"  src="{{ url("images/cards/$card.svg") }}" alt="{{$card}}"/>
+                                </div>
+                                @if(isset($playedCards[$card]))
+                                    <div class="player-name" style="color:{{$nicknameColors[$playedCards[$card]]}}">{{ $playedCards[$card] }}</div>
+                                @endif
+                            </div>
+                        </td>
+                    @endforeach
+                    <td>
+                        <div class="played-cards-amount">{{$playedCardsAmount[$suit]}}<span>/13</span></div>
+                    </td>
+                </tr>
             @endforeach
-        </div>
-    @endforeach
-</div>
+        </table>
+
+        <table>
+            <tr>
+                <th>Player Name</th>
+                <th>Points</th>
+            </tr>
+            @foreach($playerPoints as $player => $points)
+                <tr>
+                    <td style="color:{{$nicknameColors[$player]}};" >{{ $player }}:</td>
+                    <td style="border: solid;background-color:black;color:{{$nicknameColors[$player]}};" class="player-points">{{$points}}</td>
+                </tr>
+            @endforeach
+        </table>
+    </div>
 </body>
 </html>
